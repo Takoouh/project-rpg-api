11d1658489476ce92f05b567bc770d3b8b9a3dca
Florentin <florentin.coyard@gmail.com>
2022-06-28 14:59:47 +0200

    feat(minor): add config for heroku deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DB_URL,
+  password: process.env.POSTGRES_PASSWORD,
   entities: [process.env.ENTITY_PATH],
   synchronize: true,
 };
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3000;
   app.enableCors();
   // app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.warn(`App listening on http://localhost:${PORT}`);
+  });
 }
 bootstrap();
